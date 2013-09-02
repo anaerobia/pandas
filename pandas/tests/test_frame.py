8de0e45aaa3d871f2196ce7a2bfb4b49b42c107b
Phillip Cloud <cpcloud@gmail.com>
2013-09-02 18:00:22 -0400

    ENH: add in, not in, and string/list query support

--- pandas/tests/test_frame.py
+++ pandas/tests/test_frame.py
@@ -11310,6 +11310,7 @@ ENGINES = 'python', 'numexpr'
 
 class TestDataFrameQueryStrings(object):
     def check_str_query_method(self, parser, engine):
+        skip_if_no_ne()
         skip_if_no_pandas_parser(parser)
         df = DataFrame(randn(10, 1), columns=['b'])
         df['strings'] = Series(list('aabbccddee'))
@@ -11327,6 +11328,7 @@ class TestDataFrameQueryStrings(object):
             yield self.check_str_list_query_method, parser, engine
 
     def check_str_list_query_method(self, parser, engine):
+        skip_if_no_ne()
         skip_if_no_pandas_parser(parser)
         df = DataFrame(randn(10, 1), columns=['b'])
         df['strings'] = Series(list('aabbccddee'))
@@ -11355,6 +11357,7 @@ class TestDataFrameQueryStrings(object):
 
         res = df['["a", "b"] == strings']
         assert_frame_equal(res, expect)
+
 
 class TestDataFrameEvalNumExprPandas(unittest.TestCase):
     @classmethod
